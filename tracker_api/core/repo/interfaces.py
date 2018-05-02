9b83bd3a3410b5e50b4f6a343a726b6505a3dab8
Gleb Linnik <gleb_linnik@icloud.com>
2018-05-02 23:39:12 +0300

    repos interfaces implemented

--- tracker_api/core/repo/interfaces.py
+++ tracker_api/core/repo/interfaces.py
@@ -1,12 +1,59 @@
 from abc import ABCMeta, abstractmethod
 
 
-class BoardRepo(metaclass=ABCMeta):
+class BoardsRepo(metaclass=ABCMeta):
 
     @abstractmethod
-    def get(self, board_id):
+    def list_boards(self):
         pass
 
     @abstractmethod
-    def set(self, board):
+    def add_board(self, board):
         pass
+
+    @abstractmethod
+    def delete_board(self, board_id):
+        pass
+
+    @abstractmethod
+    def modify_board(self, board):
+        pass
+
+
+class ListsRepo(metaclass=ABCMeta):
+
+    @abstractmethod
+    def list_lists(self, board_id):
+        pass
+
+    @abstractmethod
+    def add_list(self, my_list, board_id):
+        pass
+
+    @abstractmethod
+    def delete_list(self, list_id):
+        pass
+
+    @abstractmethod
+    def modify_list(self, my_list):
+        pass
+
+
+class CardsRepo(metaclass=ABCMeta):
+
+    @abstractmethod
+    def list_cards(self, list_id):
+        pass
+
+    @abstractmethod
+    def add_card(self, card, list_id):
+        pass
+
+    @abstractmethod
+    def delete_card(self, card_id):
+        pass
+
+    @abstractmethod
+    def modify_card(self, card):
+        pass
+
