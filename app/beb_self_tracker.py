1de8fe6b9b29899bc05983ae4651ddd5e2c23e0a
Gleb Linnik <gleb_linnik@mail.ru>
2018-08-17 02:59:10 +0300

    Card CLI complete

--- app/beb_self_tracker.py
+++ app/beb_self_tracker.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from datetime import datetime
 
 
@@ -11,22 +12,44 @@ def validate_date(s):
 
 
 class CLIParser:
+
     def __init__(self):
-        self.parser = argparse.ArgumentParser(description='BEBSelf issues tracker')
-        self.subparsers = self.parser.add_subparsers(description='Objects to operate', dest='object')
+        self.parser = argparse.ArgumentParser(description='Achieve your goals with BebSelf issues tracker')
+        self.object_subparsers = self.parser.add_subparsers(dest='object',
+                                                            metavar="<object>",
+                                                            title="available objects are")
+        self.object_subparsers.required = True
 
+        self._add_all_parsers()
+
+    def _add_all_parsers(self):
         self._add_card_parser()
-        self._add_list_parser()
 
-    def parse(self, args=None):
-        return self.parser.parse_args(args)
+    def parse(self):
+        args = self.parser.parse_args()
+        return args
+
+# Available objects are:
+#    board     Operate boards. Boards contain lists of cards
+#    list      Operate lists. Lists contain cards.
+#    card      Operate cards. Cards are the basic structure that's meant to represent one task
+#    user      Operate users. Users may own and have different access to cards, lists, etc.
 
     def _add_card_parser(self):
-        card_parser = self.subparsers.add_parser('card', description='Operations with card entities')
-        card_subparser = card_parser.add_subparsers(dest='action')
+        card_parser = self.object_subparsers.add_parser('card',
+                                                        description="Operate cards. Cards are the basic structure "
+                                                                    "that's meant to represent one task",
+                                                        help="Operate cards. Cards are the basic structure "
+                                                                    "that's meant to represent one task")
+        card_subparser = card_parser.add_subparsers(dest='command',
+                                                    metavar="<command>",
+                                                    title="commands that applicable to cards")
+        card_subparser.required = True
 
-        card_display_parser = card_subparser.add_parser('display', description='Display card')
+        card_display_parser = card_subparser.add_parser('display', description='Display card', help='Display card')
         card_display_group = card_display_parser.add_mutually_exclusive_group()
+        card_display_group.required = True
+
         card_display_group.add_argument('-i', '--id', help='unique id of card')
         card_display_group.add_argument('-n', '--name', help='name of card (if not unique, all variants with ids '
                                                              'would be displayed)')
@@ -38,7 +61,7 @@ class CLIParser:
         card_deletion_group.add_argument('-n', '--name', help='name of card (if not unique, all variants with ids '
                                                               'would be displayed)')
 
-        cards_creation_parser = card_subparser.add_parser('create', description='Create card')
+        cards_creation_parser = card_subparser.add_parser('create', description='Create card', help='Create card')
         cards_creation_parser.add_argument('list_id', help='unique id of list where the card would be stored')
         cards_creation_parser.add_argument('name', help='name of the new card')
         cards_creation_parser.add_argument('-d', '--description', help='description of the task in the card')
@@ -48,7 +71,7 @@ class CLIParser:
                                            help="The Expiration Date - format YYYY-MM-DD,hh:mm",
                                            type=validate_date)
 
-        card_editing_parser = card_subparser.add_parser('edit', description='Change card')
+        card_editing_parser = card_subparser.add_parser('edit', description='Change card', help='Change card')
         card_editing_parser.add_argument('-n', '--name')
         card_editing_parser.add_argument('-d', '--description')
         card_editing_parser.add_argument('-p', '--priority', choices=['low', 'medium', 'high'], default='medium',
@@ -56,49 +79,9 @@ class CLIParser:
         card_editing_parser.add_argument("-e", "--expiration_date",
                                          help="The Expiration Date - format YYYY-MM-DD,hh:mm",
                                          type=validate_date)
-        card_editing_parser.add_argument('task_id')
-
-    def _add_list_parser(self):
-        list_parser = self.subparsers.add_parser('list', description='Operations with card entities')
-        card_subparser = list_parser.add_subparsers(dest='action')
-
-        card_display_parser = card_subparser.add_parser('display', description='Display card')
-        card_display_group = card_display_parser.add_mutually_exclusive_group()
-        card_display_group.add_argument('-i', '--id', help='unique id of card')
-        card_display_group.add_argument('-n', '--name', help='name of card (if not unique, all variants with ids '
-                                                             'would be displayed)')
-        card_display_parser.add_argument('-v', '--verbose', help='output details', action='store_true')
-
-        card_deletion_parser = card_subparser.add_parser('delete', description='Delete card')
-        card_deletion_group = card_deletion_parser.add_mutually_exclusive_group()
-        card_deletion_group.add_argument('-i', '--id', help='unique id of card')
-        card_deletion_group.add_argument('-n', '--name', help='name of card (if not unique, all variants with ids '
-                                                              'would be displayed)')
-
-        cards_creation_parser = card_subparser.add_parser('create', description='Create card')
-        cards_creation_parser.add_argument('list_id', help='unique id of list where the card would be stored')
-        cards_creation_parser.add_argument('name', help='name of the new card')
-        cards_creation_parser.add_argument('-d', '--description', help='description of the task in the card')
-        cards_creation_parser.add_argument('-p', '--priority', choices=['low', 'medium', 'high'], default='medium',
-                                           help='priority of the card (default is "medium")')
-        cards_creation_parser.add_argument("-e", "--expiration_date",
-                                           help="The Expiration Date - format YYYY-MM-DD,hh:mm",
-                                           type=validate_date)
-
-        card_editing_parser = card_subparser.add_parser('edit', description='Change card')
-        card_editing_parser.add_argument('-n', '--name')
-        card_editing_parser.add_argument('-d', '--description')
-        card_editing_parser.add_argument('-p', '--priority', choices=['low', 'medium', 'high'], default='medium',
-                                         help='priority of the card (default is "medium")')
-        card_editing_parser.add_argument("-e", "--expiration_date",
-                                         help="The Expiration Date - format YYYY-MM-DD,hh:mm",
-                                         type=validate_date)
-        card_editing_parser.add_argument('task_id')
-
-
-def main():
-    CLIParser().parse('card create Hello 123 -e 2017-06-01,21:10'.split())
+        card_editing_parser.add_argument('card_id')
 
 
 if __name__ == '__main__':
-    main()
+    sys.argv = "beb -h".split()
+    CLIParser().parse()
